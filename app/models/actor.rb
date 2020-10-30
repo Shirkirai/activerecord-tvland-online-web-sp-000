0603ee180ea03258b339f3d1175a1c2d7390f0e4
Jung Lee <lee.jung.s95@gmail.com>
2020-10-30 03:46:28 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -15,7 +15,7 @@ class Actor < ActiveRecord::Base
 
     def list_roles
         binding.pry
-        character = self.characters map {|col_name| col_name.name}
+        character = self.characters.map {|col_name| col_name.name}
         show = self.shows.map {|col_name| col_name.name}
 
 
