3a4d3884a3338a5bb4a869d0de15e07a87df44df
Jung Lee <lee.jung.s95@gmail.com>
2020-10-30 03:13:58 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,10 +1,11 @@
 class Actor < ActiveRecord::Base
     has_many :characters
+    has_many :shows, through: :characters
 
 
 
     def full_name
-        return self
+        return self.first_name 
     end
 
 end
