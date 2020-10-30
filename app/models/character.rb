c0929c0a7108c599c8d00bf46700d972025837cf
Jung Lee <lee.jung.s95@gmail.com>
2020-10-30 03:59:42 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,7 +4,7 @@ class Character < ActiveRecord::Base
   #binding.pry
 
   def say_that_thing_you_say
-      "#{self.name} always say: #{self.catchphrase}"
+      "#{self.name} always says: #{self.catchphrase}"
   end
 
 end
