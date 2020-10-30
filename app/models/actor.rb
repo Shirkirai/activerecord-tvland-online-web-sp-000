7fbab983df831a3096afbe74e0f417dda8e9c3f2
Jung Lee <lee.jung.s95@gmail.com>
2020-10-30 03:18:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,8 @@ class Actor < ActiveRecord::Base
     def full_name
 
         nospace_name = self.first_name + self.last_name
+
+        nospace_name.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
     end
 
 end
