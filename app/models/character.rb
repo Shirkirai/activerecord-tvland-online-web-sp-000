144127297ffe6980dbcc4089f020b0661bd0b023
Jung Lee <lee.jung.s95@gmail.com>
2020-10-30 03:54:12 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,5 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
-  def say_that_thing_you_say
-      character = self.name
-      "#{character} always says: #{character.catchphrase}"
-  end
+ 
 end
